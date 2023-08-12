378d793a05eba0d1d3165e715532e414ab9f1daf
Imani <imanimk@gmail.com>
2023-08-12 02:09:46 -0700

    first day of C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+include <unistd.h>
+int main (void)
+/**
+* main entry point 
+*return 1 writes output to standard error
+/
+{
+write (2"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n ");
+return 1;
+}
