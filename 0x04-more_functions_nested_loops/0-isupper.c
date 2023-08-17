96ccab75331a3eedc5d6c060c3a6c38c37c83b5a
imanimk <imanimkingsley@gmail.com>
2023-08-17 06:29:20 -0700

    more variables

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - check the code
+ *_isupper - checks for uppercase character.
+*/
+int _putchar(char c);
+int _isupper(int c);
+
+int main(void)
+
+{
+
+	char c;
+
+if
+	c="A"
+
+	_putchar("%c: %d\n",c, _isupper(c));
+else
+	c="a"
+
+	_putchar("%c: %d\n",c, _isupper(c));
+
+return:(0);
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains more of what ive learnt in C functions and variables.
