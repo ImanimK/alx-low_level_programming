c8975b2d172304a7d4739edcc759cd956e8e1493
imanimk <imanimkingsley@gmail.com>
2023-08-22 05:38:14 -0700

    pointers,arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+
+#define INT_MAX 2147483647
+#define INT_MIN (-2147483647 - 1)
 
 /**
  * _atoi - Converts a string to an integer.
@@ -13,30 +17,30 @@ int _atoi(char *s)
 	int i = 0;
 
 	if (s[i] == '-')
-		{
-		sign = -1;
-		i++;
-		}
+	{
+	sign = -1;
+	i++;
+	}
 	else if (s[i] == '+')
-		{
-		i++;
-		}
+	{
+	i++;
+	}
 
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
-		{
-		if (sign == 1)
-			return (INT_MAX);
-		else
-			return (INT_MIN);
-		}
+			{
+			if (sign == 1)
+			return INT_MAX;
+			else
+			return INT_MIN;
+			}
 
-		result = result * 10 + (s[i] - '0');
+	result = result * 10 + (s[i] - '0');
 		}
-		else
+	else
 		{
 		break;
 		}
