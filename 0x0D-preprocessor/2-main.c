decf5f13ab37143797aff0f3d05750d9243a3569
imanimk <imanimk@github.com>
2023-09-11 17:01:46 +0300

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the current source file.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
